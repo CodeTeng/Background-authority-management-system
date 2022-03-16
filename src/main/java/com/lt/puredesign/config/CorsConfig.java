3e8f45e22947f6ec9d2c2778b5e21738bc56c369
liteng <liteng2002823@outlook.com>
2022-03-16 22:16:37 +0800

    CorsConfig.java

--- src/main/java/com/lt/puredesign/config/CorsConfig.java
+++ src/main/java/com/lt/puredesign/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
     /**
      * 当前跨域请求最大有效时长。这里默认1天
      */
-    private static final long MAX_AGE = 24 * 60 * 60;
+    private static final long MAX_AGE = 24 * 60 * 60L;
 
     @Bean
     public CorsFilter corsFilter() {
